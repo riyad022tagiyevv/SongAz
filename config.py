44f4dfe73b08281ecc7c3fa26845a4c5aaf12ff8
RiyadMusic <106539099+RiyadMusic@users.noreply.github.com>
2023-02-26 04:05:44 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 
 class Config:
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    API_ID = int(os.environ.get("API_ID", "19485442"))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_OWNER = os.environ.get("BOT_OWNER", "")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-    PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME", "")
-    CHANNEL = os.environ.get("CHANNEL", "")
-    PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID", ""))
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6267740327:AAHr_dFL-zoi-tGo3TJP0KezzH9c3EJCK1c")
+    API_ID = int(os.environ.get("API_ID", "29138904"))
+    API_HASH = os.environ.get("API_HASH", "0cdbc65bf2e0984ad00464dfd871e90c")
+    BOT_OWNER = os.environ.get("BOT_OWNER", "RiyadAndMe")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "DTGSONG")
+    PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME", "RiyaddBlogg")
+    CHANNEL = os.environ.get("CHANNEL", "RiyaddBlogg")
+    PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID", "-1001726242069"))
